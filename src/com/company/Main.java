dc8dd412a96d3f4e10d9a0d550c76f759e064456
dylan <duhadm19@wfu.edu>
2020-12-02 18:51:55 -0500

    Made method for setting transaction class but needs to be fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,11 +16,60 @@ public class Main {
         String prefixString = new String(new char[prefix]).replace('\0', '0');
 
         Transaction firstTransaction = new Transaction();
+        userFillTransaction(firstTransaction);
         Block genesisBlock = new Block(firstTransaction, "0", new Date().getTime());
         blockchain.add(genesisBlock);
         //genesisBlock.mineBlock(prefix);
         blockTextOutput(genesisBlock, 1);
 
+
+    }
+    public static void userFillTransaction(Transaction t) {
+
+        // setting artifact
+        t.getArtifact().setArtifactID("58367561");
+        t.getArtifact().setArtifactName("Mona Lisa");
+
+        // setting country of artifact
+        t.getArtifact().getArtifactCountry().setIdNumber("19583");
+        t.getArtifact().getArtifactCountry().setHolderName("America");
+        t.getArtifact().getArtifactCountry().setHolderAddress("North America");
+        t.getArtifact().getArtifactCountry().setHolderBalance(500000);
+
+        // setting currOwner of artifact
+        t.getArtifact().getCurrOwner().setIdNumber("23894");
+        t.getArtifact().getCurrOwner().setHolderName("Dylan Duhamel");
+        t.getArtifact().getCurrOwner().setHolderAddress("4518 35th St North");
+        t.getArtifact().getCurrOwner().setHolderBalance(6786);
+
+
+        // setting time
+        t.setTimestamp(LocalDateTime.now());
+
+
+        // setting stakeholder seller
+        t.getSeller().setIdNumber("17893257");
+        t.getSeller().setHolderName("Matt Zamp");
+        t.getSeller().setHolderAddress("1220 Kress Dr");
+        t.getSeller().setHolderBalance(56321);
+
+
+        // setting stakeholder buyer
+        t.getBuyer().setIdNumber("89012365");
+        t.getBuyer().setHolderName("Nick Barsanti");
+        t.getBuyer().setHolderAddress("1573 North Pelham St");
+        t.getBuyer().setHolderBalance(3421);
+
+
+        // setting stakeholder auctionHouse
+        t.getAuctionHouse().setIdNumber("45671204");
+        t.getAuctionHouse().setHolderName("Media Sellers");
+        t.getAuctionHouse().setHolderAddress("567 West 45th St");
+        t.getAuctionHouse().setHolderBalance(678231);
+
+
+        // setting transaction price
+        t.setPrice(3423);
 
     }
 
