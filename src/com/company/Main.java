395b823fdf0e9cbc3284cc84dea83b5d1202e0cb
dylan <duhadm19@wfu.edu>
2020-12-02 19:06:58 -0500

    Working text file output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,53 +19,74 @@ public class Main {
         userFillTransaction(firstTransaction);
         Block genesisBlock = new Block(firstTransaction, "0", new Date().getTime());
         blockchain.add(genesisBlock);
-        //genesisBlock.mineBlock(prefix);
+        genesisBlock.mineBlock(prefix);
         blockTextOutput(genesisBlock, 1);
 
 
     }
     public static void userFillTransaction(Transaction t) {
 
-        // setting artifact
-        t.getArtifact().setArtifactID("58367561");
-        t.getArtifact().setArtifactName("Mona Lisa");
+        // making an artifact
+        Artifact userArt = new Artifact();
+        userArt.setArtifactID("58367561");
+        userArt.setArtifactName("Mona Lisa");
 
         // setting country of artifact
-        t.getArtifact().getArtifactCountry().setIdNumber("19583");
-        t.getArtifact().getArtifactCountry().setHolderName("America");
-        t.getArtifact().getArtifactCountry().setHolderAddress("North America");
-        t.getArtifact().getArtifactCountry().setHolderBalance(500000);
+        Stakeholder country = new Stakeholder();
+        country.setIdNumber("19583");
+        country.setHolderName("America");
+        country.setHolderAddress("North America");
+        country.setHolderBalance(500000);
 
         // setting currOwner of artifact
-        t.getArtifact().getCurrOwner().setIdNumber("23894");
-        t.getArtifact().getCurrOwner().setHolderName("Dylan Duhamel");
-        t.getArtifact().getCurrOwner().setHolderAddress("4518 35th St North");
-        t.getArtifact().getCurrOwner().setHolderBalance(6786);
+        Stakeholder currOwner = new Stakeholder();
+        currOwner.setIdNumber("23894");
+        currOwner.setHolderName("Dylan Duhamel");
+        currOwner.setHolderAddress("4518 35th St North");
+        currOwner.setHolderBalance(6786);
 
+        // adding country and currOwner to artifact
+        userArt.setArtifactCountry(country);
+        userArt.setCurrOwner(currOwner);
+
+        // ADDING ARTIFACT TO TRANSACTION
+        t.setArtifact(userArt);
 
         // setting time
         t.setTimestamp(LocalDateTime.now());
 
 
-        // setting stakeholder seller
-        t.getSeller().setIdNumber("17893257");
-        t.getSeller().setHolderName("Matt Zamp");
-        t.getSeller().setHolderAddress("1220 Kress Dr");
-        t.getSeller().setHolderBalance(56321);
+        // making a new seller
+        Stakeholder userSeller = new Stakeholder();
+        userSeller.setIdNumber("17893257");
+        userSeller.setHolderName("Matt Zamp");
+        userSeller.setHolderAddress("1220 Kress Dr");
+        userSeller.setHolderBalance(56321);
+
+        // ADDING SELLER TO TRANSACTION
+        t.setSeller(userSeller);
 
 
-        // setting stakeholder buyer
-        t.getBuyer().setIdNumber("89012365");
-        t.getBuyer().setHolderName("Nick Barsanti");
-        t.getBuyer().setHolderAddress("1573 North Pelham St");
-        t.getBuyer().setHolderBalance(3421);
+        // making a new buyer
+        Stakeholder userBuyer = new Stakeholder();
+        userBuyer.setIdNumber("89012365");
+        userBuyer.setHolderName("Nick Barsanti");
+        userBuyer.setHolderAddress("1573 North Pelham St");
+        userBuyer.setHolderBalance(3421);
+
+        // ADDING BUYER TO TRANSACTION
+        t.setBuyer(userBuyer);
 
 
-        // setting stakeholder auctionHouse
-        t.getAuctionHouse().setIdNumber("45671204");
-        t.getAuctionHouse().setHolderName("Media Sellers");
-        t.getAuctionHouse().setHolderAddress("567 West 45th St");
-        t.getAuctionHouse().setHolderBalance(678231);
+        // making a new auctionHouse
+        Stakeholder userAuction = new Stakeholder();
+        userAuction.setIdNumber("45671204");
+        userAuction.setHolderName("Media Sellers");
+        userAuction.setHolderAddress("567 West 45th St");
+        userAuction.setHolderBalance(678231);
+
+        // ADDING AUCTIONHOUSE TO TRANSACTION
+        t.setAuctionHouse(userAuction);
 
 
         // setting transaction price
